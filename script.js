652f5ab43b44cf1fe713245495227e85cd03bdc7
natsm <natsmarinho@gmail.com>
2023-04-17 18:43:01 -0300

    Elementos adicionados e saindo valores corretos no console

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnSubmt = document.querySelector("#btn").addEventListener('click', showResult)
+const inputName = document.querySelector("#name")
+const inputDate = document.querySelector("#birth-date")
+
+function showResult(){
+    console.log(inputName.value);
+    console.log(inputDate.value);
+}
