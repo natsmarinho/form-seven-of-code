e1dd7fdd3b8398178e4e16af2e2b4bf62080ad3a
natsm <natsmarinho@gmail.com>
2023-04-20 18:53:58 -0300

    feat:

--- script.js
+++ script.js
@@ -2,7 +2,29 @@ const btnSubmt = document.querySelector("#btn").addEventListener('click', showRe
 const inputName = document.querySelector("#name");
 const inputDate = document.querySelector("#birth-date");
 
-function showResult(){
-    console.log(inputName.value);
-    console.log(inputDate.value);
+function showResult(event){
+
+   const username = inputName.value;
+   const birthDay = inputDate.value;
+
+   localStorage.setItem("nome", JSON.stringify(username));
+   localStorage.setItem("data de nascimento", JSON.stringify(birthDay));
+   const getName = JSON.parse(localStorage.getItem('nome')) || [];;
+   const getDate = JSON.parse(localStorage.getItem('data de nascimento')) || [];
+
+   if (getName && getDate){
+    const nameElement = document.createElement("p");
+    nameElement.textContent = `Nome: ${username}`;
+
+    const dateElement = document.createElement("p");
+    dateElement.textContent = `Data de nascimento: ${birthDay}`;
+
+    const dadosDiv = document.querySelector(".show-data");
+    dadosDiv.appendChild(nameElement);
+    dadosDiv.appendChild(dateElement);
+   }
+
+   inputName.value = "";
+   inputDate.value = "";
+    
 }
